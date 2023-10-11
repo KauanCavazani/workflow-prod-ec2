91cc5f0443b9b75e33ebeab42c1da39c0f7217d9
KauanCavazani <kauan.cavazani@gmail.com>
2023-10-11 00:01:46 -0300

    add componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import component from "./components/component"
+
 function App() {
 
   return (
     <>
       <h1>Bem vindo!</h1>
+      <component texto="Eu sou um componente" />
       <p>1.0</p>
     </>
   )
--- src/components/component.jsx
+++ src/components/component.jsx
@@ -1,7 +1,7 @@
-function component() {
+function component({ texto }) {
     return (
         <>
-        
+            <p>{ texto }</p>
         </>
     )
 }
