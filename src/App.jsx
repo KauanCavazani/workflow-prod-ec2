7e329b3bf2b60cb95266793ecf148489a12fc742
KauanCavazani <kauan.cavazani@gmail.com>
2023-10-11 00:50:44 -0300

    teste deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Bem vindo!</h1>
-      <Componente texto="Eu sou um componenteee!!" />
+      <Componente texto="Eu sou um componenteee!" />
       <p>1.0</p>
     </>
   )
