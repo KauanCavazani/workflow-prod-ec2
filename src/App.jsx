a1e6b3408f54c22fcb3f3f05d87368398f9d119c
KauanCavazani <kauan.cavazani@gmail.com>
2023-10-11 18:36:31 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>Bem vindo!</h1>
-      <Componente texto="Eu sou um componenteee!!" />
+      <Componente texto="Eu sou um componenteee!!!" />
       <Componente2 texto="Eu sou o segundo componente!!" />
       <p>1.0</p>   
     </> 
